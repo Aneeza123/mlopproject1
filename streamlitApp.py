f8efc8f85923bf64e3088c79383c4cbe8839025a
Aneeza123 <zaneeza746@gmail.com>
2024-09-04 12:42:42 -0700

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 import streamlit as st
 
 
-model =joblib.load("Models/liveModelV1.pk1")
+model =joblib.load("liveModelV1.pk1")
 
 data = pd.read_csv('mobile_price_range_data.csv')
 X = data.iloc[:, :-1] #excluding the target column assuming the last column is the target
